7439dec7835067f9fba9f8e51d44ca6795f84253
Cillian <G00404411@atu.ie>
2023-10-03 15:52:27 +0100

    Exercise 2

--- src/main/java/ie/atu/cicdlab1part2/RequestController.java
+++ src/main/java/ie/atu/cicdlab1part2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.cicdlab1part2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
@@ -9,4 +10,7 @@ public class RequestController {
     public String hello(){
         return "Simple Greeting Method!!!";
     }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){return "Hello there " + name;}
 }
