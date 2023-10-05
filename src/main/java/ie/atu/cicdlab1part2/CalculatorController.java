2030392caadcabd1b2b692343c8a970126a9f9c6
Cillian <G00404411@atu.ie>
2023-10-05 14:49:52 +0100

    Finish calculator with @requestparam

--- src/main/java/ie/atu/cicdlab1part2/CalculatorController.java
+++ src/main/java/ie/atu/cicdlab1part2/CalculatorController.java
@@ -6,10 +6,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-
     @GetMapping("/calculate")
-    public String details(@RequestParam int num1, int num2){
-        return + num1 + " Your age: " + num2;
+    public Object calculate(@RequestParam int num1, int num2, String calculation){
+
+        int total = 0;
+
+        switch(calculation){
+            case "multiply" -> total = num1 * num2;
+            case "add" -> total = num1 + num2;
+            case "subtract" -> total = num1 - num2;
+            case "divide" -> total = num1 / num2;
+        }
+
+        return new math(total, calculation);
+    }
+
+    public class math{
+        private int total;
+        private String calculation;
+
+        public math(int total, String calculation) {
+            this.total = total;
+            this.calculation = calculation;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public String getCalculation() {
+            return calculation;
+        }
     }
 
 }
