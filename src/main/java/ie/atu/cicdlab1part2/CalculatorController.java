19635611b69d5c7024aabcaa8219c5babd32bf07
Cillian <G00404411@atu.ie>
2023-10-05 14:01:05 +0100

    Start Calcualtor

---
+++ src/main/java/ie/atu/cicdlab1part2/CalculatorController.java
@@ -0,0 +1,14 @@
+package ie.atu.cicdlab1part2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/calculate")
+    public String details(@RequestParam int num1, int num2){
+        return "Your name: "+ name + " Your age: " + age;
+    }
+}
